9a12d9ebacbae343d49eef8546759156ab9ced7f
fmcsilva <uc2018281434@student.uc.pt>
2019-11-01 17:51:08 +0000

    nao alterei nada

--- EX2.m
+++ EX2.m
@@ -3,8 +3,8 @@ function [e] = EX2(info, frequencias, alfabeto)
         alfabeto = unique(info);
     end
     %calculo da matriz de ocurrencias
-% % %     frequencias = histcounts(info(:), vertcat(alfabeto, alfabeto(length(alfabeto))+1));
     %calculo da matriz de probabilidade de cada elemento de info
+    
     prob = frequencias / numel(info);
     e = entropia(prob);
 end
